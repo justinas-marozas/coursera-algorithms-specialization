026cc30a8b96c9eced77e66bc3e9726b4bec0514
Justinas Marozas <justinas.marozas@gmail.com>
2020-01-25 20:25:48 +0000

    solve pair of closest points when best pair is split between divisions in the recursion algorithm

--- 1-algorithms-divide-conquer/week-2/closest-pair-of-points/closest_pair.py
+++ 1-algorithms-divide-conquer/week-2/closest-pair-of-points/closest_pair.py
@@ -24,11 +24,14 @@ def recursive(
     if len(points) == 2:
         return points, get_distance(*points)
     points_a, points_b = split(points)
-    return min([
-        recursive(points_a, best_pair, best_distance),
-        recursive(points_b, best_pair, best_distance),
+    best_pair_a, best_dist_a = recursive(points_a, best_pair, best_distance)
+    best_pair_b, best_dist_b = recursive(points_b, best_pair, best_distance)
+    best_pair, best_dist = min([
+        (best_pair_a, best_dist_a),
+        (best_pair_b, best_dist_b),
         (best_pair, best_distance)
     ], key=lambda x: x[1])
+    return find_closest_split_pair(points_a, points_b, best_pair, best_dist)
 
 
 def naive(points: List[Point]) -> Tuple[List[Point], float]:
@@ -48,11 +51,44 @@ def get_distance(a: Point, b: Point) -> float:
 
 
 def sort_points(points: List[Point]) -> List[Point]:
-    sorted_y = sorted(points, key=lambda p: p.y)
-    sorted_xy = sorted(sorted_y, key=lambda p: p.x)
-    return sorted_xy
+    sorted_x = sorted(points, key=lambda p: p.x)
+    sorted_yx = sorted(sorted_x, key=lambda p: p.y)
+    return sorted_yx
 
 
 def split(points: List[Point]) -> Tuple[List[Point], List[Point]]:
     split_point = math.ceil(len(points) / 2)
     return points[:split_point], points[split_point:]
+
+
+def find_closest_split_pair(
+    points_a: List[Point],
+    points_b: List[Point],
+    best_pair: List[Point],
+    best_distance: float
+) -> Tuple[List[Point], float]:
+    candidates = find_potential_split_pairs(points_a, points_b, best_distance)
+    for i, _ in enumerate(candidates):
+        for j in range(i + 1, min(i + 7, len(candidates))):
+            dist = get_distance(candidates[i], candidates[j])
+            if dist < best_distance:
+                best_distance = dist
+                best_pair = [candidates[i], candidates[j]]
+    return best_pair, best_distance
+
+
+def find_potential_split_pairs(
+    points_a: List[Point],
+    points_b: List[Point],
+    best_distance: float
+) -> List[Point]:
+    x_division = max(points_a, key=lambda p: p.x).x
+    return [
+        point
+        for point in points_a
+        if point.x > x_division - best_distance
+    ] + [
+        point
+        for point in points_b
+        if point.x < x_division + best_distance
+    ]
--- 1-algorithms-divide-conquer/week-2/closest-pair-of-points/test_closest_pair.py
+++ 1-algorithms-divide-conquer/week-2/closest-pair-of-points/test_closest_pair.py
@@ -1,13 +1,15 @@
 from typing import List
 
 import nose
-
+import numpy as np
 from parameterized import parameterized
+from scipy import spatial
 
 import closest_pair
 from closest_pair import Point
 
 
+# Closest pair is in one of the recursion divisions.
 POINTS_1: List[Point] = [
     Point(x=73.24, y=43.09),
     Point(x=7.19, y=92.69),
@@ -16,15 +18,40 @@ POINTS_1: List[Point] = [
     Point(x=102.97, y=27.57),
     Point(x=21.52, y=96.38)
 ]
-CLOSEST_DISTANCE_1 = 14.797
 CLOSEST_PAIR_1: List[Point] = [
     Point(x=7.19, y=92.69),
     Point(x=21.52, y=96.38)
 ]
+CLOSEST_DISTANCE_1 = 14.797465999
+
+# Closest pair is split between multiple recursion divisions.
+POINTS_2: List[Point] = [
+    Point(x=91.05, y=47.1),
+    Point(x=52.28, y=74.25),
+    Point(x=51.45, y=82.87),
+    Point(x=88.68, y=112.45),
+    Point(x=52.18, y=96.39),
+    Point(x=19.59, y=89.68),
+    Point(x=97.95, y=115.67),
+    Point(x=83.82, y=65.75),
+    Point(x=99.89, y=52.11),
+    Point(x=1.71, y=98.47),
+    Point(x=44.53, y=120.39),
+    Point(x=89.8, y=30.52),
+    Point(x=44.58, y=71.62),
+    Point(x=105.48, y=95.92),
+    Point(x=103.84, y=70.26)
+]
+CLOSEST_PAIR_2: List[Point] = [
+    Point(x=52.28, y=74.25),
+    Point(x=44.58, y=71.62)
+]
+CLOSEST_DISTANCE_2 = 8.13676
 
 
 @parameterized([
     (POINTS_1, CLOSEST_DISTANCE_1),
+    (POINTS_2, CLOSEST_DISTANCE_2)
 ])
 def test_closest_pair_distance_computed_correctly(points: List[Point], expected: float) -> None:
     _, actual = closest_pair.find_closest_pair(points)
@@ -34,11 +61,29 @@ def test_closest_pair_distance_computed_correctly(points: List[Point], expected:
 
 @parameterized([
     (POINTS_1, CLOSEST_PAIR_1),
+    (POINTS_2, CLOSEST_PAIR_2)
 ])
 def test_correct_closest_pair_found(points: List[Point], expected: List[Point]) -> None:
     actual, _ = closest_pair.find_closest_pair(points)
 
-    assert (actual == expected) or (reversed(actual) == expected)
+    assert (actual == expected) or (list(reversed(actual)) == expected)
+
+
+def test_with_large_random_input() -> None:
+    points_np = np.random.uniform(-128., 128., size=(3333, 2))
+    dist_matrix = spatial.distance_matrix(points_np, points_np)
+    expected_dist = dist_matrix[dist_matrix > 0.].min()
+    point_a_idx, point_b_idx = np.where(dist_matrix == expected_dist)[0]
+    points = [
+        Point(x=point[0], y=point[1])
+        for point in points_np
+    ]
+    expected_pair = [points[point_a_idx], points[point_b_idx]]
+
+    actual_pair, actual_dist = closest_pair.find_closest_pair(points)
+
+    assert (actual_pair == expected_pair) or (list(reversed(actual_pair)) == expected_pair)
+    assert round(actual_dist, 3) == round(expected_dist, 3)
 
 
 if __name__ == '__main__':
