bea5b348d5ebb40e10c910f893fdc18a1eb2af6e
Justinas Marozas <justinas.marozas@gmail.com>
2020-01-30 22:15:59 +0000

    make quicksort partition function accept the partition index

--- 1-algorithms-divide-conquer/week-3/quicksort/quicksort.py
+++ 1-algorithms-divide-conquer/week-3/quicksort/quicksort.py
@@ -1,7 +1,6 @@
-from typing import List
 
 
-def partition(seq: List, l_i: int, r_i: int) -> List:
+def partition(seq: list, l_i: int, r_i: int, p_i: int) -> list:
     """Partition a part of the given sequence.
 
     Choose an item `p` to partition by and move all items `x < p` to the left of `p`
@@ -9,8 +8,12 @@ def partition(seq: List, l_i: int, r_i: int) -> List:
     :param seq: The sequence to partition.
     :param l_i: The leftmost index.
     :param r_i: The rightmost index.
+    :param p_i: Partition index.
     :returns: Same sequence with the part bounded by `l_i` and `r_i` partitioned.
     """
+    # Lets start by moving the partition key to the front of the sequence.
+    # Just so it's easier to keep track of it as items get moved around.
+    seq[p_i], seq[l_i] = seq[l_i], seq[p_i]
     p = seq[l_i]
     p_i = l_i
     i = p_i + 1
--- 1-algorithms-divide-conquer/week-3/quicksort/test_quicksort.py
+++ 1-algorithms-divide-conquer/week-3/quicksort/test_quicksort.py
@@ -1,18 +1,23 @@
 import nose
+from parameterized import parameterized
 
 import quicksort
 
 
-def test_partition() -> None:
-    seq = [3, 8, 2, 5, 1, 4, 7, 6]
+@parameterized([
+    ([3, 8, 2, 5, 1, 4, 7, 6], 0, 7, 0, 2),
+    ([3, 8, 2, 5, 1, 4, 7, 6], 0, 7, 3, 4)
+])
+def test_partition(seq: list, l_i: int, r_i: int, p_i: int, p_i_post: int) -> None:
+    pivot = seq[p_i]
+    actual = quicksort.partition(seq, l_i, r_i, p_i)
 
-    actual = quicksort.partition(seq, 0, 7)
-
-    pivot = 3
-    pos = 2  # index of pivot value
-    assert actual[pos] == pivot
-    assert all(x < actual[pos] for x in actual[:pos])
-    assert all(x > actual[pos] for x in actual[pos + 1:])
+    print(f'{actual = }')
+    print(f'{p_i_post = }')
+    print(f'{pivot = }')
+    assert actual[p_i_post] == pivot
+    assert all(x < actual[p_i_post] for x in actual[:p_i_post])
+    assert all(x > actual[p_i_post] for x in actual[p_i_post + 1:])
 
 
 if __name__ == '__main__':
