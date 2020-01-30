852a494306392489c985074ef003f11beab6de3c
Justinas Marozas <justinas.marozas@gmail.com>
2020-01-30 22:26:53 +0000

    add more quicksort partition tests

--- 1-algorithms-divide-conquer/week-3/quicksort/test_quicksort.py
+++ 1-algorithms-divide-conquer/week-3/quicksort/test_quicksort.py
@@ -5,16 +5,20 @@ import quicksort
 
 
 @parameterized([
+    # Basic example.
     ([3, 8, 2, 5, 1, 4, 7, 6], 0, 7, 0, 2),
-    ([3, 8, 2, 5, 1, 4, 7, 6], 0, 7, 3, 4)
+    # Go through sorting steps.
+    ([3, 8, 2, 5, 1, 4, 7, 6], 0, 7, 3, 4),
+    ([4, 2, 3, 1, 5, 8, 7, 6], 0, 5, 2, 2),
+    ([1, 2, 3, 4, 5, 8, 7, 6], 5, 7, 7, 5),
+    ([1, 2, 3, 4, 5, 6, 7, 8], 0, 2, 1, 1),
+    ([1, 2, 3, 4, 5, 6, 7, 8], 6, 7, 6, 6)
 ])
 def test_partition(seq: list, l_i: int, r_i: int, p_i: int, p_i_post: int) -> None:
     pivot = seq[p_i]
+
     actual = quicksort.partition(seq, l_i, r_i, p_i)
 
-    print(f'{actual = }')
-    print(f'{p_i_post = }')
-    print(f'{pivot = }')
     assert actual[p_i_post] == pivot
     assert all(x < actual[p_i_post] for x in actual[:p_i_post])
     assert all(x > actual[p_i_post] for x in actual[p_i_post + 1:])
