989f9e1468edb7f6aacccfcabdf0acc44437710c
Justinas Marozas <justinas.marozas@gmail.com>
2020-03-01 22:21:25 +0000

    implement Karger min cut algorithm

---
+++ 1-algorithms-divide-conquer/week-4/karger-min-cut/mincut.py
@@ -0,0 +1,86 @@
+import random
+from typing import Generator, List, Tuple
+
+
+RawVertex = Tuple[int, List[int]]
+Edge = Tuple[int, int]
+
+
+def main() -> None:
+    vertices = read_file('vertices.txt')
+    edges = convert_to_edges(vertices)
+    graph = list(edges)
+    cuts = [
+        random_contraction(graph)
+        for _ in range(count_vertices(graph))
+    ]
+    lengths = [len(cut) for cut in cuts]
+    print(min(lengths))
+
+
+def read_file(filepath: str) -> Generator[RawVertex, None, None]:
+    for line in open(filepath, mode='r', encoding='utf-8'):
+        line = line.strip()
+        vertices = [
+            int(label)
+            for label in line.split('\t')
+        ]
+        yield vertices[0], vertices[1:]
+
+
+def convert_to_edges(vertices: Generator[RawVertex, None, None]) -> Generator[Edge, None, None]:
+    for vertex in vertices:
+        yield from vertex_to_edge(vertex)
+
+
+def vertex_to_edge(vertex: RawVertex) -> Generator[Edge, None, None]:
+    u, vs = vertex
+    for v in vs:
+        yield u, v
+
+
+def random_contraction(graph: List[Edge]) -> List[Edge]:
+    """
+    While there are more than 2 vertices:
+    * Pick a random edge;
+    * Merge vertices of the picked edge;
+    * Remove self-loops;
+    """
+    while count_vertices(graph) > 2:
+        pivot = random.choice(graph)
+        graph = merge_vertices(graph, pivot)
+        graph = remove_self_loops(graph)
+    return graph
+
+
+def count_vertices(graph: List[Edge]) -> int:
+    labels = set()
+    for u, v in graph:
+        labels.add(u)
+        labels.add(v)
+    return len(labels)
+
+
+def merge_vertices(graph: List[Edge], pivot: Edge) -> List[Edge]:
+    new_label = min(pivot)
+    for i, edge in enumerate(graph):
+        u, v = edge
+        if u in pivot:
+            u = new_label
+        if v in pivot:
+            v = new_label
+        if (u, v) != edge:
+            graph[i] = (u, v)
+    return graph
+
+
+def remove_self_loops(graph: List[Edge]) -> List[Edge]:
+    return [
+        (u, v)
+        for u, v in graph
+        if u != v
+    ]
+
+
+if __name__ == '__main__':
+    main()
---
+++ 1-algorithms-divide-conquer/week-4/karger-min-cut/test_mincut.py
@@ -0,0 +1,147 @@
+from typing import List
+
+import pytest
+
+import mincut
+
+
+class TestReadFile:
+
+    @pytest.fixture
+    def file_mock(self, mocker):
+        fn = mocker.patch.object(mincut, 'open')
+        fn.return_value = (
+            '\t'.join(str(x) for x in range(i, 5, -1))
+            for i in range(10, 50)
+        )
+        return fn
+
+    @pytest.fixture
+    def actual(self, file_mock) -> List[mincut.RawVertex]:
+        return list(mincut.read_file('asd'))
+
+    def test_expected_structure_is_returned(self, actual: List[mincut.RawVertex]) -> None:
+        for u, vs in actual:
+            assert isinstance(u, int)
+            assert isinstance(vs, list)
+
+    def test_expected_values_are_returned(self, actual: List[mincut.RawVertex]) -> None:
+        expected = [
+            (i, list(range(i - 1, 5, -1)))
+            for i in range(10, 50)
+        ]
+        for (exp_u, exp_vs), (act_u, act_vs) in zip(expected, actual):
+            assert act_u == exp_u
+            assert act_vs == exp_vs
+
+
+class TestConvertToEdges:
+
+    def test_vertices_converted_to_correct_edges(self) -> None:
+        vertices = [
+            (1, [2, 3, 4]),
+            (2, [1, 5, 7]),
+            (3, [1, 4]),
+            (4, [1, 3, 5, 6, 7]),
+            (5, [2, 4, 7]),
+            (6, [4, 7, 8]),
+            (7, [2, 4, 5, 6, 8]),
+            (8, [6, 7]),
+        ]
+        expected = [
+            (1, 2), (1, 3), (1, 4),
+            (2, 1), (2, 5), (2, 7),
+            (3, 1), (3, 4),
+            (4, 1), (4, 3), (4, 5), (4, 6), (4, 7),
+            (5, 2), (5, 4), (5, 7),
+            (6, 4), (6, 7), (6, 8),
+            (7, 2), (7, 4), (7, 5), (7, 6), (7, 8),
+            (8, 6), (8, 7),
+        ]
+        actual = mincut.convert_to_edges(vertices)
+        actual = list(actual)
+        assert set(actual) == set(expected)
+
+
+class TestCountVertices:
+
+    def test_vertices_are_counted_correctly(self) -> None:
+        graph = [
+            (1, 2), (1, 3), (1, 4),
+            (2, 1), (2, 5), (2, 7),
+            (3, 1), (3, 4),
+            (4, 1), (4, 3), (4, 5), (4, 6), (4, 7),
+            (5, 2), (5, 4), (5, 7),
+            (6, 4), (6, 7), (6, 8),
+            (7, 2), (7, 4), (7, 5), (7, 6), (7, 8),
+            (8, 6), (8, 7),
+        ]
+        actual = mincut.count_vertices(graph)
+        assert actual == 8
+
+
+class TestRemoveSelfLoops:
+
+    def test_self_loops_removed(self) -> None:
+        graph = [
+            (1, 2), (1, 1), (1, 3), (1, 4),
+            (2, 1), (2, 5), (2, 2), (2, 7),
+            (3, 1), (3, 4), (3, 3),
+        ]
+        expected = [
+            (1, 2), (1, 3), (1, 4),
+            (2, 1), (2, 5), (2, 7),
+            (3, 1), (3, 4),
+        ]
+        actual = mincut.remove_self_loops(graph)
+        assert actual == expected
+
+
+class TestContractVertices:
+
+    def test_pivot_edges_get_merged(self) -> None:
+        graph = [
+            (1, 2), (1, 3), (1, 4),
+            (2, 1), (2, 5), (2, 7),
+            (3, 1), (3, 4),
+            (4, 1), (4, 3), (4, 5), (4, 6), (4, 7),
+            (5, 2), (5, 4), (5, 7),
+            (6, 4), (6, 7), (6, 8),
+            (7, 2), (7, 4), (7, 5), (7, 6), (7, 8),
+            (8, 6), (8, 7),
+        ]
+        pivot = (3, 1)
+        expected = [
+            (1, 2), (1, 1), (1, 4),
+            (2, 1), (2, 5), (2, 7),
+            (1, 1), (1, 4),
+            (4, 1), (4, 1), (4, 5), (4, 6), (4, 7),
+            (5, 2), (5, 4), (5, 7),
+            (6, 4), (6, 7), (6, 8),
+            (7, 2), (7, 4), (7, 5), (7, 6), (7, 8),
+            (8, 6), (8, 7),
+        ]
+
+        actual = mincut.merge_vertices(graph, pivot)
+
+        assert set(actual) == set(expected)
+
+
+class TestRandomContraction:
+
+    def test_it(self) -> None:
+        graph = [
+            (1, 2), (1, 3), (1, 4),
+            (2, 1), (2, 5), (2, 7),
+            (3, 1), (3, 4),
+            (4, 1), (4, 3), (4, 5), (4, 6), (4, 7),
+            (5, 2), (5, 4), (5, 7),
+            (6, 4), (6, 7), (6, 8),
+            (7, 2), (7, 4), (7, 5), (7, 6), (7, 8),
+            (8, 6), (8, 7),
+        ]
+
+        actual = mincut.random_contraction(graph)
+
+        assert mincut.count_vertices(actual) == 2
+        assert all(u != v for u, v in actual)
